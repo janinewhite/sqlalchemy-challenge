df204c6900bae7764d59bc9fc55bd31cb31c15a6
Janine White <janine.white@gmail.com>
2019-10-27 19:45:01 -0700

    API working

--- app.py
+++ app.py
@@ -25,11 +25,12 @@ app = Flask(__name__)
 def home():
     return(
         "Available Routes:<br/>"
-        "/api/v1.0/precipitation Precipitation by Station and Date<br/>"
-        "/api/v1.0/station Stations<br/>"
-        "/api/v1.0/tobs Temperatures for the previous year<br/>"
-        "/api/v1.0/<start> Minimum temperature, the average temperature, and the max temperature for a given start date<br/>"
-        "/api/v1.0/<start>/<end> Minimum temperature, the average temperature, and the max temperature for between start date< and end date<br/>"
+        "<table><thead><tr><th>Endpoint</th><th>Returns</th></tr></thead>"
+        "<tr><td>/api/v1.0/precipitation</td><td>Precipitation by Station and Date</td></tr>"
+        "<tr><td>/api/v1.0/station</td><td>Stations</td></tr>"
+        "<tr><td>/api/v1.0/tobs</td><td>Temperatures for the previous year</td></tr>"
+        "<tr><td>/api/v1.0/&lt;start date&gt;</td><td>Minimum temperature, the average temperature, and the max temperature for a given start date (YYYY-MM-DD)</td></tr>"
+        "<tr><td>/api/v1.0/&lt;start date&gt;/&lt;end date&gt;</td><td>Minimum temperature, the average temperature, and the max temperature for between start date and end date (YYYY-MM-DD)</td></tr></table>"
     )
 
 def precipitation_results_to_list(results):
@@ -39,7 +40,6 @@ def precipitation_results_to_list(results):
         measure["date"] = date
         measure["precipitation"] = precipitation
         measure["station"] = station
-        print(measure)
         measures.append(measure)
     return measures
 
@@ -59,7 +59,6 @@ def station_results_to_list(results):
         station_dict["latitude"] = latitude
         station_dict["longitude"] = longitude
         station_dict["elevation"] = elevation
-        print(station_dict)
         stations.append(station_dict)
     return stations
 
@@ -84,25 +83,35 @@ def temp_results_to_list(results):
         measure["date"] = date
         measure["temperature"] = temperature
         measure["station"] = station
-        print(measure)
         temperatures.append(measure)
     return temperatures
 
 @app.route("/api/v1.0/tobs")
+def temps():
     session = Session(engine)
     max_date = engine.execute('SELECT MAX(date) FROM measurement').fetchall()
     last_date = string_to_date(max_date[0][0])
     first_date = one_year_before(last_date)
-    results = session.query(Measurement.date,Measurement.tobs.label('temperature'),Measurement.station).where(Measurement.date >= first_date).all()
+    results = session.query(Measurement.date,Measurement.tobs.label('temperature'),Measurement.station).filter(Measurement.date >= first_date).all()
     session.close()
     return jsonify(temp_results_to_list(results))
 
 def check_date(text):
     try:
-        date = string_to_date(text)
+        try_date = string_to_date(text)
         return True
     except:
         return False
+
+def check_range(text):
+    max_date = engine.execute('SELECT MAX(date) FROM measurement').fetchall()
+    max_date_string = str(max_date[0][0])
+    min_date = engine.execute('SELECT MIN(date) FROM measurement').fetchall()
+    min_date_string = str(min_date[0][0])
+    if not ((text >= min_date_string) & (text <= max_date_string)):
+        return f"Start must be between {min_date_string} and {max_date_string}."
+    else:
+        return ""
 
 def temp_results_to_statistics(results):
     statistics = {}
@@ -111,28 +120,34 @@ def temp_results_to_statistics(results):
     statistics["Maximum Temperature"] = results[0][2]
     statistics["From Date"] = results[0][3]
     statistics["To Date"] = results[0][4]
-    print(statistics)
     return statistics
 
 @app.route("/api/v1.0/<start>")
-    def temperatures_by_start_date(start):
-        good_start = check_date(start)
-        if good_start:
+def temperatures_by_start_date(start):
+    if check_date(start):
+        if check_range(start):
             session = Session(engine)
-            results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs),func.min(Measurement.date),func.max(Mesurement.date)).filter(Measurement.date >= start).all()
+            results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs),func.min(Measurement.date),func.max(Measurement.date)).filter(Measurement.date >= start).all()
             session.close()
             return jsonify(temp_results_to_statistics(results))
         else:
-            return jsonify({"error": f"{start} is not a date."}), 404
+           return jsonify({"error": f"{start} is not a date in the database."}), 404 
+    else:
+        return jsonify({"error": f"{start} is not a date."}), 404
 
-@app.route("/api/v1.0/<start>/<end>")
-    def temperatures_by_start_date(start):
-        good_start = check_date(start)
-        good_end = check_date(end)
-        if good_start and good_end:
-            session = Session(engine)
-            results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs),func.min(Measurement.date),func.max(Mesurement.date)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-            session.close()
-            return jsonify(temp_results_to_statistics(results))
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def temperatures_by_start_and_end_date(start_date,end_date):
+    if check_date(start_date) and check_date(end_date):
+        if end_date >= start_date:
+            range_error  = check_range(start_date)
+            if range_error=="":
+                session = Session(engine)
+                results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs),func.min(Measurement.date),func.max(Measurement.date)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+                session.close()
+                return jsonify(temp_results_to_statistics(results))
+            else:
+                return jsonify({"error": range_error}), 404 
         else:
-            return jsonify({"error": f"{start} and/or {end} is not a date."}), 404
+            return jsonify({"error": f"{start_date} is after {end_date}."}), 404
+    else:
+        return jsonify({"error": f"{start_date} and/or {end_date} are not a date."}), 404
